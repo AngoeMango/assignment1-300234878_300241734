a9b1ecf9f2867ffc999b29e112687d8411f4a203
Natasa <natasa.bolic@gmail.com>
2023-10-02 21:56:35 -0400

    added PointCP2 and PointCP3 as subclasses in the design5 folder

---
+++ pointcp/design5/PointCP2.java
@@ -0,0 +1,96 @@
+package design5;
+	// This file contains material supporting section 2.9 of the textbook:
+    // "Object Oriented Software Engineering" and is issued under the open-source
+    // license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+import design3.PointCP3;
+
+/**
+     * This class contains instances of coordinates in polar format.  
+     * It also provides the utilities to convert them into cartesian format.
+     * It is not an optimal design, it is used only to compare design implementations.
+     *
+     * @author Fran&ccedil;ois B&eacute;langer
+     * @author Dr Timothy C. Lethbridge
+     * @version July 2000
+     */
+public class PointCP2 extends PointCP5
+    {
+    //Instance variables ************************************************
+    
+    /**
+     * Contains the current value of RHO.
+     */
+    private double rho;
+    
+    /**
+     * Contains the current value of THETA.
+     */
+    private double theta;
+        
+    
+    //Constructors ******************************************************
+
+    /**
+     * Constructs a coordinate object.
+     */
+    public PointCP2(double rho, double theta)
+    {
+        this.rho = rho;
+        this.theta = theta;
+    }
+        
+    
+    //Instance methods **************************************************
+    
+    
+    public double getX()
+    {
+        return (Math.cos(Math.toRadians(theta)) * rho);
+    }
+    
+    public double getY()
+    {
+        return (Math.sin(Math.toRadians(theta)) * rho);
+    }
+    
+    public double getRho()
+    {
+        return rho;
+    }
+    
+    public double getTheta()
+    {
+        return theta;
+    }
+    
+    /**
+     * Converts Polar coordinates to Cartesian coordinates.
+     */
+    public PointCP3 convertStorageToCartesian()
+    {
+    	return new PointCP3(getX(), getY());
+    }
+    
+    /**
+     * Factory method (used to create a new point so that 
+     * rotatePoint can be implemented concretely in the superclass).
+     *
+     * @param value1 The value of rho.
+     * @param value2 The value of theta.
+     * @return The point characterized by the two values.
+     */
+    public PointCP5 newPt(double value1, double value2) {
+    	return new PointCP2(value1, value2);
+    }
+
+    /**
+     * Returns information about the coordinates.
+     *
+     * @return A String containing information about the coordinates.
+     */
+    public String toString()
+    {
+        return "Stored as Polar [" + getRho() + "," + getTheta() + "]" + "\n";
+    }
+}
---
+++ pointcp/design5/PointCP3.java
@@ -0,0 +1,95 @@
+package design5;
+
+// This file contains material supporting section 2.9 of the textbook:
+    // "Object Oriented Software Engineering" and is issued under the open-source
+    // license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+    /**
+     * This class contains instances of coordinates in cartesian format.
+     * It also provides the utilities to convert them into polar format.
+     * It is not an optimal design, it is used to compare design implementations.
+     *
+     * @author Fran&ccedil;ois B&eacute;langer
+     * @author Dr Timothy C. Lethbridge
+     * @version July 2000
+     */
+public class PointCP3 extends PointCP5
+    {
+    //Instance variables ************************************************
+    
+    /**
+     * Contains the current value of X.
+     */
+    private double x;
+    
+    /**
+     * Contains the current value of Y.
+     */
+    private double y;
+        
+    
+    //Constructors ******************************************************
+
+    /**
+     * Constructs a coordinate object.
+     */
+    public PointCP3(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+        
+    
+    //Instance methods **************************************************
+    
+    
+    public double getX()
+    {
+        return x;
+    }
+    
+    public double getY()
+    {
+        return y;
+    }
+    
+    public double getRho()
+    {
+        return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+    }
+    
+    public double getTheta()
+    {
+        return Math.toDegrees(Math.atan2(y, x));
+    }
+    
+    /**
+     * Converts Cartesian coordinates to Polar coordinates.
+     */
+    public PointCP2 convertStorageToPolar()
+    {
+        return new PointCP2(getRho(), getTheta());
+    }
+    
+    /**
+     * Factory method (used to create a new point so that 
+     * rotatePoint can be implemented concretely in the superclass).
+     *
+     * @param value1 The value of x.
+     * @param value2 The value of y.
+     * @return The point characterized by the two values.
+     */
+    public PointCP5 newPt(double value1, double value2) {
+    	return new PointCP3(value1, value2);
+    }
+
+    /**
+     * Returns information about the coordinates.
+     *
+     * @return A String containing information about the coordinates.
+     */
+    public String toString()
+    {
+        return "Stored as Cartesian  (" + getX() + "," + getY() + ")" + "\n";
+    }
+}
